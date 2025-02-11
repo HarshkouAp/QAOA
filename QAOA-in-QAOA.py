099f1173f2a35ff2d2973764674272954984e84d
Alexey <Harshkouap@gmail.com>
2025-02-11 15:37:40 +0300

    QAOA-in-QAOA функция

--- QAOA-in-QAOA.py
+++ QAOA-in-QAOA.py
@@ -1,7 +1,6 @@
+from Graph_generator import *
 from QAOA import QAOA
-from Graph_generator import generate_graph
 import networkx as nx
-import cvxgraphalgs as cvxgr
 import numpy as np
 import pandas as pd
 import warnings
@@ -9,235 +8,221 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-def Get_connected_subgraphs(max_size):
 
-    global Layer, Graph_division, Graph_nodes
-
-    connected_components = list(nx.connected_components(G))
-    subgraphs = []
-
-    for component in connected_components:
-        g = G.subgraph(component)
-        nodes = list(g.nodes)
-
-        visited = set()
-        for i in range(len(nodes)):
-            if nodes[i] in visited:
-                continue
-            subgraph_nodes = []
-            stack = [nodes[i]]
-
-            while stack and len(subgraph_nodes) < max_size:
-                node = stack.pop()
-                if node not in visited:
-                    visited.add(node)
-                    subgraph_nodes.append(node)
-                    for neighbor in g.neighbors(node):
-                        if neighbor not in visited:
-                            stack.append(neighbor)
-
-            if len(subgraph_nodes) > 1:
-                subgraph = g.subgraph(subgraph_nodes)
-                subgraphs.append(list(subgraph.edges))
-
-    iterator = 1
-    node_name = max(list(G.nodes()))
-    layer_edges = {}
-    layer_nodes = {}
-
-    for sg in subgraphs:
-
-        H = nx.Graph()
-        H.add_edges_from(sg)
-        G.add_node(node_name + iterator)
-        layer_edges[node_name + iterator] = list(H.edges())
-        layer_nodes[node_name + iterator] = list(H.nodes())
-
-        for i in list(H.nodes()):
-            neighbors = list(G.neighbors(i))
-
-            for k in neighbors:
-                if k not in list(H.nodes()):
-                    G.remove_edge(i, k)
-                    G.add_edge(k, node_name + iterator)
-            G.remove_node(i)
-        iterator += 1
-
-    Graph_division[Layer] = layer_edges
-    Graph_nodes[Layer] = layer_nodes
-    Layer += 1
+def QAOA2(graph, subgraphsize, depth=10, max_iter=10000, visualise=False, callback=False):
 
 
-def Subgraph_solution(layer):
+    def Get_connected_subgraphs(max_size):
+        global Layer, Graph_division, Graph_nodes
 
-    solution_dict = {}
-    sub = Graph_division[layer]
+        connected_components = list(nx.connected_components(graph))
+        subgraphs = []
 
-    if layer == 0:
-        for node in sub.keys():
+        for component in connected_components:
+            g = graph.subgraph(component)
+            nodes = list(g.nodes)
+
+            visited = set()
+            for i in range(len(nodes)):
+                if nodes[i] in visited:
+                    continue
+                subgraph_nodes = []
+                stack = [nodes[i]]
+
+                while stack and len(subgraph_nodes) < max_size:
+                    node = stack.pop()
+                    if node not in visited:
+                        visited.add(node)
+                        subgraph_nodes.append(node)
+                        for neighbor in g.neighbors(node):
+                            if neighbor not in visited:
+                                stack.append(neighbor)
+
+                if len(subgraph_nodes) > 1:
+                    subgraph = g.subgraph(subgraph_nodes)
+                    subgraphs.append(list(subgraph.edges))
+
+        iterator = 1
+        node_name = max(list(graph.nodes()))
+        layer_edges = {}
+        layer_nodes = {}
+
+        for sg in subgraphs:
+
             H = nx.Graph()
-            H.add_edges_from(sub[node])
-            old_nodes = list(H.nodes())
-            new_nodes = list(range(len(old_nodes)))
-            mapping = dict(zip(old_nodes, new_nodes))
-            H = nx.relabel_nodes(H, mapping)
-            R, S, P, E, C_s, C_e, Pt_Q, Pt_C = QAOA(H, 15)
-            solution_dict[node] = S[0]
-    else:
-        for node in sub.keys():
-            H = nx.Graph()
-            H.add_weighted_edges_from(sub[node])
-            old_nodes = list(H.nodes())
-            new_nodes = list(range(len(old_nodes)))
-            mapping = dict(zip(old_nodes, new_nodes))
-            H = nx.relabel_nodes(H, mapping)
-            R, S, P, E, C_s, C_e, Pt_Q, Pt_C = QAOA(H, 15, weighted=True)
-            solution_dict[node] = S[0]
+            H.add_edges_from(sg)
+            graph.add_node(node_name + iterator)
+            layer_edges[node_name + iterator] = list(H.edges())
+            layer_nodes[node_name + iterator] = list(H.nodes())
 
-    Sub_solution[layer] = solution_dict
+            for i in list(H.nodes()):
+                neighbors = list(graph.neighbors(i))
 
+                for k in neighbors:
+                    if k not in list(H.nodes()):
+                        graph.remove_edge(i, k)
+                        graph.add_edge(k, node_name + iterator)
+                graph.remove_node(i)
+            iterator += 1
 
-def Weights(layer):
-
-    sub_edges = Graph_division[layer]
-    sub_nodes = Graph_nodes[layer - 1]
-
-    for node in sub_edges.keys():
-        for k, j in sub_edges[node]:
-            weight = 0
-            k_solution = Sub_solution[layer - 1][k]
-            j_solution = Sub_solution[layer - 1][j]
-
-            for k_node, j_node in zip(sub_nodes[k], sub_nodes[j]):
-                if (k_node, j_node) in A_conections:
-                    if k_solution[sub_nodes[k].index(k_node)] == j_solution[sub_nodes[j].index(j_node)]:
-                        weight += 1
-                    else:
-                        weight -= 1
-
-            Graph_division[layer][node][sub_edges[node].index((k, j))] += (weight,)
+        Graph_division[Layer] = layer_edges
+        Graph_nodes[Layer] = layer_nodes
+        Layer += 1
 
 
-def Recursion(max_size):
+    def Subgraph_solution(layer):
+        solution_dict = {}
+        sub = Graph_division[layer]
 
-    while 1:
-        Get_connected_subgraphs(max_size)
-        if len(G.nodes()) <= max_size:
-            Graph_division[Layer] = {"F": list(G.edges())}
-            Graph_nodes[Layer] = {"F": list(G.nodes())}
-            break
-
-    for layer in Graph_division.keys():
-        if layer != max(Graph_division.keys()):
-            Subgraph_solution(layer)
-            Weights(layer + 1)
+        if layer == 0:
+            for node in sub.keys():
+                H = nx.Graph()
+                H.add_edges_from(sub[node])
+                old_nodes = list(H.nodes())
+                new_nodes = list(range(len(old_nodes)))
+                mapping = dict(zip(old_nodes, new_nodes))
+                H = nx.relabel_nodes(H, mapping)
+                R, S, P, E, C_s, C_e, Pt_Q, Pt_C = QAOA(H, depth, max_iter=max_iter)
+                solution_dict[node] = S[0]
         else:
-            Subgraph_solution(layer)
+            for node in sub.keys():
+                H = nx.Graph()
+                H.add_weighted_edges_from(sub[node])
+                old_nodes = list(H.nodes())
+                new_nodes = list(range(len(old_nodes)))
+                mapping = dict(zip(old_nodes, new_nodes))
+                H = nx.relabel_nodes(H, mapping)
+                R, S, P, E, C_s, C_e, Pt_Q, Pt_C = QAOA(H, depth, weighted=True, max_iter=max_iter)
+                solution_dict[node] = S[0]
+
+        Sub_solution[layer] = solution_dict
 
 
-def Reformulate_solution():
+    def Weights(layer):
+        sub_edges = Graph_division[layer]
+        sub_nodes = Graph_nodes[layer - 1]
 
-    for layer in list(Sub_solution.keys())[-1:0:-1]:
-        for node in Sub_solution[layer].keys():
-            sub = Sub_solution[layer][node]
-            for ind in range(len(sub)):
-                if sub[ind] == "1":
-                    subnodes = Graph_nodes[layer][node]
-                    Sub_solution[layer - 1][subnodes[ind]] = Invert(Sub_solution[layer - 1][subnodes[ind]])
+        for node in sub_edges.keys():
+            for k, j in sub_edges[node]:
+                weight = 0
+                k_solution = Sub_solution[layer - 1][k]
+                j_solution = Sub_solution[layer - 1][j]
 
+                for k_node, j_node in zip(sub_nodes[k], sub_nodes[j]):
+                    if (k_node, j_node) in A_conections:
+                        if k_solution[sub_nodes[k].index(k_node)] == j_solution[sub_nodes[j].index(j_node)]:
+                            weight += 1
+                        else:
+                            weight -= 1
 
-def Invert(solution):
-
-    inv_solution = ""
-    for i in solution:
-        if i == "1":
-            inv_solution += "0"
-        else:
-            inv_solution += "1"
-
-    return inv_solution
+                Graph_division[layer][node][sub_edges[node].index((k, j))] += (weight,)
 
 
-def Answer():
+    def Recursion(max_size):
+        while 1:
+            Get_connected_subgraphs(max_size)
+            if len(graph.nodes()) <= max_size:
+                Graph_division[Layer] = {"F": list(graph.edges())}
+                Graph_nodes[Layer] = {"F": list(graph.nodes())}
+                break
 
-    ans = []
-
-    for node in Sub_solution[0]:
-        for ind in range(len(Sub_solution[0][node])):
-            ans.append([Graph_nodes[0][node][ind], Sub_solution[0][node][ind]])
-
-    ans = np.array(ans)
-    Df = pd.DataFrame(ans, columns=["node", "solution"])
-    Df["node"] = Df["node"].apply(lambda x: int(x))
-    Df.sort_values("node", inplace=True)
-
-    ans = ""
-    for i in Df["solution"]:
-        ans += i
-
-    return ans
+        for layer in Graph_division.keys():
+            if layer != max(Graph_division.keys()):
+                Subgraph_solution(layer)
+                Weights(layer + 1)
+            else:
+                Subgraph_solution(layer)
 
 
-def Goemans_williamson_weighted():
-    sdp_cut = cvxgr.algorithms.goemans_williamson_weighted(A)
-    return sdp_cut.evaluate_cut_size(A)
+    def Reformulate_solution():
+        for layer in list(Sub_solution.keys())[-1:0:-1]:
+            for node in Sub_solution[layer].keys():
+                sub = Sub_solution[layer][node]
+                for ind in range(len(sub)):
+                    if sub[ind] == "1":
+                        subnodes = Graph_nodes[layer][node]
+                        Sub_solution[layer - 1][subnodes[ind]] = Invert(Sub_solution[layer - 1][subnodes[ind]])
 
 
-def Classical_solution(state):
-    cut = 0
-    for k, j in A.edges():
-        if state[k] != state[j]:
-            cut += 1
-    return cut
+    def Invert(solution):
+        inv_solution = ""
+        for i in solution:
+            if i == "1":
+                inv_solution += "0"
+            else:
+                inv_solution += "1"
+
+        return inv_solution
 
 
-G = generate_graph(20, 0.5, visualise=True)
-A = G.copy()
-A_conections = list(A.edges())
+    def Answer():
+        ans = []
+
+        for node in Sub_solution[0]:
+            for ind in range(len(Sub_solution[0][node])):
+                ans.append([Graph_nodes[0][node][ind], Sub_solution[0][node][ind]])
+
+        ans = np.array(ans)
+        Df = pd.DataFrame(ans, columns=["node", "solution"])
+        Df["node"] = Df["node"].apply(lambda x: int(x))
+        Df.sort_values("node", inplace=True)
+
+        ans = ""
+        for i in Df["solution"]:
+            ans += i
+
+        return ans
 
 
-Layer = 0
-Graph_division = {}
-Graph_nodes = {}
-Sub_solution = {}
-
-# try:
-#
-#     Recursion(5)
-#     Reformulate_solution()
-#     QAOA_ans = Answer()
-#     QAOA_energy = Classical_solution(QAOA_ans)
-#     C_energy = Goemans_williamson_weighted()
-#
-#     print(Graph_nodes)
-#
-#     M = nx.Graph()
-#     M.add_weighted_edges_from(Graph_division[1]["F"])
-#     options = {'node_size': 1000, 'width': 1}
-#     nx.draw(M, nx.circular_layout(M), with_labels=False, **options)
-#     nx.draw_networkx_edges(M, nx.circular_layout(M), edgelist=M.edges(), width=1)
-#     edge_labels = nx.get_edge_attributes(M, "weight")
-#     nx.draw_networkx_edge_labels(M, nx.circular_layout(M), edge_labels, font_size=16)
-#     print(dict(zip(range(len(Graph_nodes[1]["F"])), Graph_nodes[1]["F"])))
-#     node_lab = dict(zip(Graph_nodes[1]["F"], Graph_nodes[1]["F"]))
-#     nx.draw_networkx_labels(M, nx.circular_layout(M), node_lab, font_size=16, font_color="white")
-#
-#     ax = plt.gca()
-#     ax.margins(0.08)
-#     plt.axis("off")
-#     plt.show()
-#
-#     print("-------------------------------------")
-#     print(f"N_nodes : {20}   seed : random   max_size : {6}")
-#     print(f"GW : {C_energy}   QAOA : {QAOA_energy}")
-#
-# except Exception:
-#
-#     print("-------------------------------------")
-#     print("ERROR")
+    def Classical_solution(state):
+        cut = 0
+        for k, j in A.edges():
+            if state[k] != state[j]:
+                cut += 1
+        return cut
 
 
+    A = graph.copy()
+    A_conections = list(A.edges())
 
+    global Graph_division, Graph_nodes, Sub_solution, Layer
+    Layer = 0
+    Graph_division = {}
+    Graph_nodes = {}
+    Sub_solution = {}
+
+    Recursion(subgraphsize)
+    Reformulate_solution()
+    QAOA_ans = Answer()
+    QAOA_energy = Classical_solution(QAOA_ans)
+
+    if visualise:
+
+        M = nx.Graph()
+        M.add_weighted_edges_from(Graph_division[1]["F"])
+        options = {'node_size': 1000, 'width': 1}
+        nx.draw(M, nx.circular_layout(M), with_labels=False, **options)
+        nx.draw_networkx_edges(M, nx.circular_layout(M), edgelist=M.edges(), width=1)
+        edge_labels = nx.get_edge_attributes(M, "weight")
+        nx.draw_networkx_edge_labels(M, nx.circular_layout(M), edge_labels, font_size=16)
+        # print(dict(zip(range(len(Graph_nodes[1]["F"])), Graph_nodes[1]["F"])))
+        node_lab = dict(zip(Graph_nodes[1]["F"], Graph_nodes[1]["F"]))
+        nx.draw_networkx_labels(M, nx.circular_layout(M), node_lab, font_size=16, font_color="white")
+
+        ax = plt.gca()
+        ax.margins(0.08)
+        plt.axis("off")
+        plt.show()
+
+    if callback:
+
+        print("-------------------------------------")
+        print(f"N_nodes : {20}   seed : random   max_size : {6}")
+        print(f"Energy : {QAOA_energy}")
+
+
+# G = generate_graph(20, 0.5, visualise=True)
+# QAOA2(G, 5, 10, callback=True, visualise=True)
 
 
 
