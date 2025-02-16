e949599ca7fc685628590455fd8064f5e2fca43e
Alexey <Harshkouap@gmail.com>
2025-02-17 00:36:32 +0300

    Новый способ разбиения

--- Graph_cutter.py
+++ Graph_cutter.py
@@ -1,86 +1,150 @@
 import networkx as nx
+from networkx.algorithms import community
 import matplotlib.pyplot as plt
 from Graph_generator import generate_graph
 
 
-def get_connected_subgraphs(max_size):
-    global Layer
-    connected_components = list(nx.connected_components(G))
+def split_graph_into_subgraphs(graph, max_size):
+    """
+    Разбивает граф на подграфы, размер которых не превышает max_size.
+    Старается максимизировать связность внутри подграфов и избегать подграфов с одной вершиной.
+
+    :param graph: Исходный граф (networkx.Graph)
+    :param max_size: Максимальный размер подграфа
+    :return: Два списка:
+             1. Список подграфов в виде списков связей (рёбер).
+             2. Список подграфов в виде списков вершин.
+    """
+    # Используем алгоритм обнаружения сообществ (например, greedy modularity)
+    communities = list(community.greedy_modularity_communities(graph))
+
     subgraphs = []
 
-    for component in connected_components:
-        g = G.subgraph(component)
-        nodes = list(g.nodes)
+    # Проходим по каждому сообществу и разбиваем его на подграфы, если оно слишком большое
+    for comm in communities:
+        comm_subgraph = graph.subgraph(comm)
 
-        # Перебираем узлы, чтобы создать подграфы
-        visited = set()
-        for i in range(len(nodes)):
-            if nodes[i] in visited:
-                continue
-            subgraph_nodes = []
-            stack = [nodes[i]]
+        # Если размер сообщества меньше или равен max_size, добавляем его как есть
+        if len(comm_subgraph) <= max_size:
+            subgraphs.append(comm_subgraph)
+        else:
+            # Если сообщество слишком большое, разбиваем его на части
+            # Используем алгоритм k-связных компонент или просто разбиваем на равные части
+            # В данном примере разбиваем на равные части
+            nodes = list(comm_subgraph.nodes())
+            for i in range(0, len(nodes), max_size):
+                subgraph_nodes = nodes[i:i + max_size]
+                subgraph = graph.subgraph(subgraph_nodes)
+                subgraphs.append(subgraph)
 
-            # Используем DFS для получения связного подграфа
-            while stack and len(subgraph_nodes) < max_size:
-                node = stack.pop()
-                if node not in visited:
-                    visited.add(node)
-                    subgraph_nodes.append(node)
-                    for neighbor in g.neighbors(node):
-                        if neighbor not in visited:
-                            stack.append(neighbor)
+    # Убедимся, что подграфы не содержат изолированных вершин (если это возможно)
+    # Если есть подграфы с одной вершиной, попробуем объединить их с соседними подграфами
+    final_subgraphs = []
+    single_node_subgraphs = []
 
-            # Если подграф больше 1, добавляем его
-            if len(subgraph_nodes) > 1:
-                subgraph = g.subgraph(subgraph_nodes)
-                subgraphs.append(list(subgraph.edges))
+    for subgraph in subgraphs:
+        if len(subgraph) == 1:
+            single_node_subgraphs.append(subgraph)
+        else:
+            final_subgraphs.append(subgraph)
 
-    Graph_division[Layer] = subgraphs
-    Layer += 1
+    # Объединяем подграфы с одной вершиной с соседними подграфами
+    for single_subgraph in single_node_subgraphs:
+        node = list(single_subgraph.nodes())[0]
+        neighbors = list(graph.neighbors(node))
 
-    iterator = 1
-    node_name = max(list(G.nodes()))
-    for sg in subgraphs:
+        # Ищем соседний подграф, к которому можно добавить вершину
+        merged = False
+        for i, final_subgraph in enumerate(final_subgraphs):
+            if any(neighbor in final_subgraph for neighbor in neighbors):
+                # Добавляем вершину к этому подграфу, если это не нарушает max_size
+                if len(final_subgraph) < max_size:
+                    final_subgraphs[i] = graph.subgraph(list(final_subgraph.nodes()) + [node])
+                    merged = True
+                    break
 
-        H = nx.Graph()
-        H.add_edges_from(sg)
-        G.add_node(node_name + iterator)
-        new_noda_dict[str(node_name + iterator)] = list(H.nodes())
+        # Если не удалось объединить, добавляем как отдельный подграф
+        if not merged:
+            final_subgraphs.append(single_subgraph)
 
-        for i in list(H.nodes()):
-            neighbors = list(G.neighbors(i))
+    # Преобразуем подграфы в списки связей и списки вершин
+    subgraphs_edges = []
+    subgraphs_nodes = []
 
-            for k in neighbors:
-                if k not in list(H.nodes()):
-                    G.remove_edge(i, k)
-                    cuted_edges.append([i, k])
-                    G.add_edge(k, node_name + iterator)
-            G.remove_node(i)
-        iterator += 1
+    for subgraph in final_subgraphs:
+        # Список связей (рёбер) подграфа
+        edges = list(subgraph.edges())
+        subgraphs_edges.append(edges)
+
+        # Список вершин подграфа
+        nodes = list(subgraph.nodes())
+        subgraphs_nodes.append(nodes)
+
+    return subgraphs_edges, subgraphs_nodes
 
 
-G = generate_graph(125, 0.7, visualise=True)
+def grapf_reduction(graph, max_size, visualise=False):
+    """
+    Итеративно сворачивает (редуцирует) граф.
 
-options = {'node_size': 1000, 'width': 1, 'arrowsize': 10}
+    :param graph: Исходный граф (networkx.Graph)
+    :param max_size: Максимальный размер подграфа
+    :return: Три словаря:
+             1. reduction_layers - словарь содержащий все связи графа на каждой итерации.
+                (keys = итерация, vals = связи графа)
+             2. reduction_edges - словарь содержащий связи всех подграфов на каждой итерации.
+                (keys1 = итерация, keys2 = вершина в которую сворачивается граф, vals = связи подграфа графа)
+             3. reduction_edges - словарь содержащий вершины всех подграфов на каждой итерации.
+                (keys1 = итерация, keys2 = вершина в которую сворачивается граф, vals = связи подграфа графа)
+    """
+    layer = 0
+    reduction_edges = {}
+    reduction_nodes = {}
+    reduction_layers = {}
 
-Layer = 0
-cuted_edges = []
-new_noda_dict = {}
-Graph_division = {}
+    while len(graph.nodes()) > max_size:
+        iterator = 1
+        layer_edges = {}
+        layer_nodes = {}
+        reduction_layers[layer] = list(graph.edges())
+        subgraphs_edges, subgraphs_nodes = split_graph_into_subgraphs(graph, max_size)
 
-get_connected_subgraphs(5)
-nx.draw(G, nx.circular_layout(G), with_labels=True, **options)
-ax = plt.gca()
-ax.collections[0].set_edgecolor("#000000")
-plt.show()
+        node_name = max(list(graph.nodes()))
 
-get_connected_subgraphs(5)
-Graph_division[Layer] = [list(G.edges)]
-nx.draw(G, nx.circular_layout(G), with_labels=True, **options)
-ax = plt.gca()
-ax.collections[0].set_edgecolor("#000000")
-plt.show()
+        for edges, nodes in zip(subgraphs_edges, subgraphs_nodes):
 
-print(len(Graph_division[0]))
-print(len(Graph_division[1]))
-print(len(Graph_division[2]))
+            graph.add_node(node_name + iterator)
+            layer_edges[node_name + iterator] = edges
+            layer_nodes[node_name + iterator] = nodes
+
+            for node in nodes:
+                neighbors = list(graph.neighbors(node))
+
+                for k in neighbors:
+                    if k not in nodes:
+                        graph.remove_edge(node, k)
+                        graph.add_edge(k, node_name + iterator)
+                graph.remove_node(node)
+            iterator += 1
+
+        reduction_edges[layer] = layer_edges
+        reduction_nodes[layer] = layer_nodes
+        layer += 1
+
+        if visualise:
+            options = {'node_size': 1000, 'width': 1}
+            nx.draw(graph, nx.circular_layout(graph), with_labels=True, **options)
+            nx.draw_networkx_edges(graph, nx.circular_layout(graph), edgelist=graph.edges(), width=1)
+            edge_labels = nx.get_edge_attributes(graph, "weight")
+            nx.draw_networkx_edge_labels(graph, nx.circular_layout(graph), edge_labels)
+
+            ax = plt.gca()
+            ax.margins(0.08)
+            plt.axis("off")
+            plt.show()
+
+    return reduction_layers, reduction_edges, reduction_nodes
+
+# # Пример использования
+# G = generate_graph(125, 0.7, visualise=True)  # Пример графа
+# L, E, N = grapf_reduction(G, 5, visualise=True)
