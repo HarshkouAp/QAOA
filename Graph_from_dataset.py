0ab6fbdc4daf62c4e838de8ac0fc4b877cc265a6
Alexey <Harshkouap@gmail.com>
2025-02-22 13:34:29 +0300

    Функция для парсинга графов из G датасета

---
+++ Graph_from_dataset.py
@@ -0,0 +1,16 @@
+import networkx as nx
+
+def graph_from_dataset(graph_ind):
+    graph = nx.Graph()
+
+    with open(f"Data/G_dataset/G{graph_ind}.txt", "r") as file:
+        data = file.readlines()[1::]
+        for line in data:
+            edge = line.replace("\n", "").split()
+            node_1 = edge[0]
+            node_2 = edge[1]
+            weight = edge[1]
+            graph.add_edge(node_1, node_2, weight=weight)
+    print(len(list(graph.edges())))
+    return graph
+
