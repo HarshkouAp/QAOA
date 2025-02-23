71c818d2b5346a083a7144fc39fa69847fdfe4b5
Alexey <Harshkouap@gmail.com>
2025-02-23 23:57:46 +0300

    Функция для парсинга данных из G датасета

--- Graph_from_dataset.py
+++ Graph_from_dataset.py
@@ -11,6 +11,12 @@ def graph_from_dataset(graph_ind):
             node_2 = edge[1]
             weight = edge[1]
             graph.add_edge(node_1, node_2, weight=weight)
+    with open(f"Data/G_dataset/G{graph_ind}_opt_value.txt", "r") as file:
+        data = file.readlines()[0].replace("\n", "").split()
+        optimal_cut = int(data[0])
 
-    return graph
+    return graph, optimal_cut
 
+G, C = graph_from_dataset(1)
+
+print(type(C))
