a425edfe6f9c396a19e54ad1fce8cb1f346d9d03
Alexey <Harshkouap@gmail.com>
2025-03-04 14:18:33 +0300

    Function for parsing graphs from datasets

--- Graph_from_dataset.py
+++ Graph_from_dataset.py
@@ -5,18 +5,18 @@ def graph_from_dataset(graph_ind):
 
     with open(f"Data/G_dataset/G{graph_ind}.txt", "r") as file:
         data = file.readlines()[1::]
+
         for line in data:
             edge = line.replace("\n", "").split()
-            node_1 = edge[0]
-            node_2 = edge[1]
-            weight = edge[1]
+            node_1 = int(edge[0])
+            node_2 = int(edge[1])
+            weight = float(edge[2])
             graph.add_edge(node_1, node_2, weight=weight)
+
     with open(f"Data/G_dataset/G{graph_ind}_opt_value.txt", "r") as file:
         data = file.readlines()[0].replace("\n", "").split()
         optimal_cut = int(data[0])
 
+
     return graph, optimal_cut
 
-G, C = graph_from_dataset(1)
-
-print(type(C))
